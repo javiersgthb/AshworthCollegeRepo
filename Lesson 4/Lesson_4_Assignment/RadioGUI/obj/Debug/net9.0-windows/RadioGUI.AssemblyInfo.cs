e084fc72316cda94d635a2eb93b384b5d57d782f
Theatre-Life <jsilva_junk@outlook.com>
2025-09-21 14:02:34 -0500

    chore: apply .gitignore cleanup

--- Lesson 4/Lesson_4_Assignment/RadioGUI/obj/Debug/net9.0-windows/RadioGUI.AssemblyInfo.cs
+++ Lesson 4/Lesson_4_Assignment/RadioGUI/obj/Debug/net9.0-windows/RadioGUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RadioGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab95a5ef4caf37f5f4785f7e759375de8bc6450")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb13b678949df17d48845a6ac580de35336b428")]
 [assembly: System.Reflection.AssemblyProductAttribute("RadioGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RadioGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
